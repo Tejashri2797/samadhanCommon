16214fd1bc01da99b3db66efe1ec34630b263d45
Tejashri2797 <tejnikam27@gmail.com>
2023-01-18 09:56:36 +0530

    SamadhanCollbration

---
+++ lib/View_mdal/department_list_vm.dart
@@ -0,0 +1,30 @@
+
+
+
+import 'package:get/get.dart';
+
+import '../Modal/department_report.dart';
+import '../Repository/department_report_repo.dart';
+
+
+class ReportDepartmentViewModal extends GetxController{
+  List<ReportDepartmentModalClass> departmentList=[];
+  var isLoading = true.obs;
+
+  @override
+  void onInit(){
+    super.onInit();
+    getDepartment();
+  }
+
+
+  getDepartment() async {
+    var departmentData = await ReportDepartmentRepo.getDepartmentApi();
+    if(departmentData != null){
+      departmentList = departmentData;
+      isLoading.value= false;
+    }
+    print(departmentList);
+
+  }
+}
